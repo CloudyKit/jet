2bd579245aeae12b981f83fc209bfbbf7f1d6f9c
Daniel Lohse <daniel.lohse@alfatraining.de>
2017-02-20 18:13:54 +0100

    Add tests for multi loader

---
+++ loaders/multi/multi_test.go
@@ -0,0 +1,31 @@
+package multi
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/CloudyKit/jet"
+	"github.com/CloudyKit/jet/jettest"
+	"github.com/CloudyKit/jet/loaders/httpfs"
+)
+
+func TestZeroLoaders(t *testing.T) {
+	l := NewLoader()
+	if _, err := l.Open("does-not-exist.jet"); err == nil {
+		t.Fatal("Open should have returned an error but didn't.")
+	}
+	fileName, ok := l.Exists("does-not-exists.jet")
+	if fileName != "" || ok != false {
+		t.Fatalf("Exists called on an empty file system should have returned empty and false but returned %q and %+v", fileName, ok)
+	}
+}
+
+func TestTwoLoaders(t *testing.T) {
+	osFSLoader := jet.NewOSFileSystemLoader("./testData")
+	httpFSLoader := httpfs.NewLoader(http.Dir("../../testData"))
+	l := NewLoader(osFSLoader, httpFSLoader)
+	set := jet.NewHTMLSetLoader(l)
+	jettest.RunWithSet(t, set, nil, nil, "resolve/simple.jet", "", "simple.jet")
+	jettest.RunWithSet(t, set, nil, nil, "base.jet", "", "")
+	jettest.RunWithSet(t, set, nil, nil, "simple2", "", "simple2\n")
+}
