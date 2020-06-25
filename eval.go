CloudyKit/jet

commit 2e37ce43d2525ae1d14b600f2cf865b8eeeab313

clean up 'include' parsing/execution

eval.go
@@ -165,32 +165,47 @@ func (state *Runtime) setValue(name string, val reflect.Value) bool {
 }
 
 // Resolve resolves a value from the execution context
-func (state *Runtime) Resolve(name string) reflect.Value {
+func (state *Runtime) Resolve(name string) (reflect.Value, error) {
+	v := reflect.Value{}
+	defer func() { v = indirectEface(v) }()
 
 	if name == "." {
-		return state.context
+		return state.context, nil
 	}
 
-	sc := state.scope
-	// try to resolve variables in the current scope
-	vl, ok := sc.variables[name]
-	// if not found walks parent scopes
-	for !ok && sc.parent != nil {
-		sc = sc.parent
-		vl, ok = sc.variables[name]
-	}
-
-	// if not found check globals
-	if !ok {
-		state.set.gmx.RLock()
-		vl, ok = state.set.globals[name]
-		state.set.gmx.RUnlock()
-		// not found check defaultVariables
-		if !ok {
-			vl, ok = defaultVariables[name]
+	// try current, then parent variable scopes
+	sc, ok := state.scope, false
+	for !ok && sc != nil {
+		v, ok = sc.variables[name]
+		if ok {
+			return v, nil
 		}
+		sc = sc.parent
 	}
-	return indirectEface(vl)
+
+	// try globals
+	state.set.gmx.RLock()
+	v, ok = state.set.globals[name]
+	state.set.gmx.RUnlock()
+	if ok {
+		return v, nil
+	}
+
+	// try default variables
+	v, ok = defaultVariables[name]
+	if ok {
+		return v, nil
+	}
+
+	return reflect.Value{}, fmt.Errorf("identifier '%q' not available in current (%+v) or parent scope, global, or default variables", name, state.scope.variables)
+}
+
+func (state *Runtime) MustResolve(name string) reflect.Value {
+	v, err := state.Resolve(name)
+	if err != nil {
+		panic(err)
+	}
+	return v
 }
 
 func (st *Runtime) recover(err *error) {
@@ -351,15 +366,8 @@ func (st *Runtime) executeYieldBlock(block *BlockNode, blockParam, yieldParam *B
 	}
 }
 
-func (st *Runtime) executeList(list *ListNode) reflect.Value {
-	inNewSCOPE := false
-	defer func() {
-		if inNewSCOPE {
-			st.releaseScope()
-		}
-	}()
-
-	returnValue := reflect.Value{}
+func (st *Runtime) executeList(list *ListNode) (returnValue reflect.Value) {
+	inNewScope := false // to use just one scope for multiple actions with variable declarations
 
 	for i := 0; i < len(list.Nodes); i++ {
 		node := list.Nodes[i]
@@ -375,9 +383,10 @@ func (st *Runtime) executeList(list *ListNode) reflect.Value {
 			node := node.(*ActionNode)
 			if node.Set != nil {
 				if node.Set.Let {
-					if !inNewSCOPE {
-						st.newScope() //creates new scope in the back state
-						inNewSCOPE = true
+					if !inNewScope {
+						st.newScope()
+						inNewScope = true
+						defer st.releaseScope()
 					}
 					st.executeLetList(node.Set)
 				} else {
@@ -494,44 +503,52 @@ func (st *Runtime) executeList(list *ListNode) reflect.Value {
 			st.executeYieldBlock(block, block.Parameters, block.Parameters, block.Expression, block.Content)
 		case NodeInclude:
 			node := node.(*IncludeNode)
-			var Name string
-
-			name := st.evalPrimaryExpressionGroup(node.Name)
-			if name.Type().Implements(stringerType) {
-				Name = name.String()
-			} else if name.Kind() == reflect.String {
-				Name = name.String()
-			} else {
-				node.errorf("unexpected expression type %q in template yielding", getTypeString(name))
-			}
-
-			t, err := st.set.getTemplate(Name, node.TemplateName)
-			if err != nil {
-				node.error(err)
-			} else {
-				st.newScope()
-				st.blocks = t.processedBlocks
-				var context reflect.Value
-				if node.Expression != nil {
-					context = st.context
-					st.context = st.evalPrimaryExpressionGroup(node.Expression)
-				}
-				Root := t.Root
-				for t.extends != nil {
-					t = t.extends
-					Root = t.Root
-				}
-				returnValue = st.executeList(Root)
-				st.releaseScope()
-				if node.Expression != nil {
-					st.context = context
-				}
-			}
+			returnValue = st.executeInclude(node)
 		case NodeReturn:
 			node := node.(*ReturnNode)
 			returnValue = st.evalPrimaryExpressionGroup(node.Value)
 		}
 	}
+
+	return returnValue
+}
+
+func (st *Runtime) executeInclude(node *IncludeNode) (returnValue reflect.Value) {
+	var templateName string
+	name := st.evalPrimaryExpressionGroup(node.Name)
+	if name.Type().Implements(stringerType) {
+		templateName = name.String()
+	} else if name.Kind() == reflect.String {
+		templateName = name.String()
+	} else {
+		node.errorf("evaluating name of template to include: unexpected expression type %q", getTypeString(name))
+	}
+
+	t, err := st.set.getTemplate(templateName, node.TemplateName)
+	if err != nil {
+		node.error(err)
+		return reflect.Value{}
+	}
+
+	st.newScope()
+	defer st.releaseScope()
+
+	st.blocks = t.processedBlocks
+
+	var context reflect.Value
+	if node.Expression != nil {
+		context = st.context
+		defer func() { st.context = context }()
+		st.context = st.evalPrimaryExpressionGroup(node.Expression)
+	}
+
+	Root := t.Root
+	for t.extends != nil {
+		t = t.extends
+		Root = t.Root
+	}
+
+	returnValue = st.executeList(Root)
 
 	return returnValue
 }
@@ -645,8 +662,8 @@ func (st *Runtime) isSet(node Node) (ok bool) {
 		resolved, err := resolveIndex(base, index)
 		return err == nil && notNil(resolved)
 	case NodeIdentifier:
-		value := st.Resolve(node.String())
-		return notNil(value)
+		value, err := st.Resolve(node.String())
+		return err == nil && notNil(value)
 	case NodeField:
 		node := node.(*FieldNode)
 		resolved := st.context
@@ -1006,9 +1023,9 @@ func (st *Runtime) evalBaseExpressionGroup(node Node) reflect.Value {
 	case NodeString:
 		return reflect.ValueOf(&node.(*StringNode).Text).Elem()
 	case NodeIdentifier:
-		resolved := st.Resolve(node.(*IdentifierNode).Ident)
-		if !resolved.IsValid() {
-			node.errorf("identifier '%s' is not available in the current scope (%+v)", node, st.variables)
+		resolved, err := st.Resolve(node.(*IdentifierNode).Ident)
+		if err != nil {
+			node.error(err)
 		}
 		return resolved
 	case NodeField:

default.go
@@ -81,7 +81,6 @@ func init() {
 			return reflect.Value{}
 		})),
 		"includeIfExists": reflect.ValueOf(Func(func(a Arguments) reflect.Value {
-
 			a.RequireNumOfArguments("includeIfExists", 1, 2)
 			t, err := a.runtime.set.GetTemplate(a.Get(0).String())
 			// If template exists but returns an error then panic instead of failing silently
@@ -93,22 +92,21 @@ func init() {
 			}
 
 			a.runtime.newScope()
+			defer a.runtime.releaseScope()
+
 			a.runtime.blocks = t.processedBlocks
-			Root := t.Root
+			root := t.Root
 			if t.extends != nil {
-				Root = t.extends.Root
+				root = t.extends.Root
 			}
 
 			if a.NumOfArguments() > 1 {
 				c := a.runtime.context
+				defer func() { a.runtime.context = c }()
 				a.runtime.context = a.Get(1)
-				a.runtime.executeList(Root)
-				a.runtime.context = c
-			} else {
-				a.runtime.executeList(Root)
 			}
 
-			a.runtime.releaseScope()
+			a.runtime.executeList(root)
 
 			return hiddenTRUE
 		})),
@@ -120,8 +118,12 @@ func init() {
 			}
 
 			a.runtime.newScope()
+			defer a.runtime.releaseScope()
+
 			w := a.runtime.Writer
+			defer func() { a.runtime.Writer = w }()
 			a.runtime.Writer = ioutil.Discard
+
 			a.runtime.blocks = t.processedBlocks
 			root := t.Root
 			if t.extends != nil {
@@ -130,15 +132,10 @@ func init() {
 
 			if a.NumOfArguments() > 1 {
 				c := a.runtime.context
+				defer func() { a.runtime.context = c }()
 				a.runtime.context = a.Get(1)
-				result = a.runtime.executeList(root)
-				a.runtime.context = c
-			} else {
-				result = a.runtime.executeList(root)
 			}
-
-			a.runtime.releaseScope()
-			a.runtime.Writer = w
+			result = a.runtime.executeList(root)
 
 			return result
 		})),

parse.go
@@ -375,9 +375,10 @@ func (t *Template) parseYield() Node {
 		end     Node
 	)
 
-	// content yield {{yield content}}
+	// parse block name
 	name = t.nextNonSpace()
 	if name.typ == itemContent {
+		// content yield {{yield content}}
 		if t.peekNonSpace().typ != itemRightDelim {
 			pipe = t.expression(context)
 		}
@@ -386,10 +387,24 @@ func (t *Template) parseYield() Node {
 	} else if name.typ != itemIdentifier {
 		t.unexpected(name, context)
 	}
+
+	// parse block parameters
 	bplist = t.blockParametersList(false, context)
+
+	// parse optional context & content
 	typ := t.peekNonSpace().typ
-	if typ != itemRightDelim {
-		if typ == itemContent {
+	if typ == itemRightDelim {
+		t.expect(itemRightDelim, context)
+	} else {
+		if typ != itemContent {
+			// parse context expression
+			pipe = t.expression("yield")
+			typ = t.peekNonSpace().typ
+		}
+		if typ == itemRightDelim {
+			t.expect(itemRightDelim, context)
+		} else if typ == itemContent {
+			// parse content from following nodes (until {{end}})
 			t.nextNonSpace()
 			t.expect(itemRightDelim, context)
 			content, end = t.itemList()
@@ -397,20 +412,8 @@ func (t *Template) parseYield() Node {
 				t.errorf("unexpected %s in %s", end, context)
 			}
 		} else {
-			pipe = t.expression("yield")
-			if t.peekNonSpace().typ == itemContent {
-				t.nextNonSpace()
-				t.expect(itemRightDelim, context)
-				content, end = t.itemList()
-				if end.Type() != nodeEnd {
-					t.errorf("unexpected %s in %s", end, context)
-				}
-			} else {
-				t.expect(itemRightDelim, context)
-			}
+			t.unexpected(t.nextNonSpace(), context)
 		}
-	} else {
-		t.expect(itemRightDelim, context)
 	}
 
 	return t.newYield(name.pos, t.lex.lineNumber(), name.val, bplist, pipe, content, false)
@@ -421,12 +424,11 @@ func (t *Template) parseInclude() Node {
 
 	name := t.expression("include")
 
-	if t.nextNonSpace().typ != itemRightDelim {
-		t.backup()
+	if t.peekNonSpace().typ != itemRightDelim {
 		pipe = t.expression("include")
-		t.expect(itemRightDelim, "include invocation")
-
 	}
+
+	t.expect(itemRightDelim, "include invocation")
 
 	return t.newInclude(name.Position(), t.lex.lineNumber(), name, pipe)
 }

commit 5723c2197963762b2b3b5caa2c75524ff9a27751

restore old Resolve() API & behavior
I broke this in 2e37ce43d2525ae1d14b600f2cf865b8eeeab313, forgetting it's an exported function

eval.go
@@ -160,8 +160,8 @@ func (state *Runtime) setValue(name string, val reflect.Value) {
 	panic(fmt.Errorf("could not find non-nil variable scope to set %q = %v", name, val))
 }
 
-// Resolve resolves a value from the execution context
-func (state *Runtime) Resolve(name string) (reflect.Value, error) {
+// Resolve resolves a value from the execution context.
+func (state *Runtime) resolve(name string) (reflect.Value, error) {
 	v, ok := reflect.Value{}, false
 	defer func() { v = indirectEface(v) }()
 
@@ -196,8 +196,15 @@ func (state *Runtime) Resolve(name string) (reflect.Value, error) {
 	return reflect.Value{}, fmt.Errorf("identifier %q not available in current (%+v) or parent scope, global, or default variables", name, state.scope.variables)
 }
 
+// Resolve calls resolve() and ignores any errors, meaning it may return a zero reflect.Value.
+func (state *Runtime) Resolve(name string) reflect.Value {
+	v, _ := state.resolve(name)
+	return v
+}
+
+// Resolve calls resolve() and panics if there is an error.
 func (state *Runtime) MustResolve(name string) reflect.Value {
-	v, err := state.Resolve(name)
+	v, err := state.resolve(name)
 	if err != nil {
 		panic(err)
 	}
@@ -692,7 +699,7 @@ func (st *Runtime) isSet(node Node) (ok bool) {
 		resolved, err := resolveIndex(base, index)
 		return err == nil && notNil(resolved)
 	case NodeIdentifier:
-		value, err := st.Resolve(node.String())
+		value, err := st.resolve(node.String())
 		return err == nil && notNil(value)
 	case NodeField:
 		node := node.(*FieldNode)
@@ -1053,7 +1060,7 @@ func (st *Runtime) evalBaseExpressionGroup(node Node) reflect.Value {
 	case NodeString:
 		return reflect.ValueOf(&node.(*StringNode).Text).Elem()
 	case NodeIdentifier:
-		resolved, err := st.Resolve(node.(*IdentifierNode).Ident)
+		resolved, err := st.resolve(node.(*IdentifierNode).Ident)
 		if err != nil {
 			node.error(err)
 		}