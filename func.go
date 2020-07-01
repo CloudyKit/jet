75083489f5f048c7a697abacc6c6ca9cf302f606
sauerbraten <willing.alexander@googlemail.com>
2020-07-01 21:04:40 +0200

    introduce Arguments.ParseInto() utility function
    ParseInto helps you going between actual types and reflect.Value when writing custom functions. for now, some commonly used types are supported, and more can be added later.

--- func.go
+++ func.go
@@ -17,6 +17,7 @@ package jet
 import (
 	"fmt"
 	"reflect"
+	"time"
 )
 
 // Arguments holds the arguments passed to jet.Func.
@@ -66,6 +67,91 @@ func (a *Arguments) NumOfArguments() int {
 // Runtime get the Runtime context
 func (a *Arguments) Runtime() *Runtime {
 	return a.runtime
+}
+
+// ParseInto parses the arguments into the provided pointers. It returns an error if the number of pointers passed in does not
+// equal the number of arguments, if any argument's value is invalid according to Go's reflect package, if an argument can't
+// be used as the value the pointer passed in at the corresponding position points to, or if an unhandled pointer type is encountered.
+// Allowed pointer types are pointers to interface{}, int, int64, float64, bool, string,  time.Time, reflect.Value, []interface{},
+// map[string]interface{}. If a pointer to a reflect.Value is passed in, the argument be assigned as-is to the value pointed to. For
+// pointers to int or float types, type conversion is performed automatically if necessary.
+func (a *Arguments) ParseInto(ptrs ...interface{}) error {
+	if len(ptrs) < a.NumOfArguments() {
+		return fmt.Errorf("have %d arguments, but only %d pointers to parse into", a.NumOfArguments(), len(ptrs))
+	}
+
+	for i := 0; i < a.NumOfArguments(); i++ {
+		arg, ptr := indirectEface(a.Get(i)), ptrs[i]
+		ok := false
+
+		if !arg.IsValid() {
+			return fmt.Errorf("argument at position %d is not a valid value", i)
+		}
+
+		switch p := ptr.(type) {
+		case *reflect.Value:
+			*p, ok = arg, true
+		case *int:
+			switch arg.Kind() {
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+				*p, ok = int(arg.Int()), true
+			case reflect.Float32, reflect.Float64:
+				*p, ok = int(arg.Float()), true
+			default:
+				return fmt.Errorf("could not parse %v (%s) into %v (%T)", arg, arg.Type(), ptr, ptr)
+			}
+		case *int64:
+			switch arg.Kind() {
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+				*p, ok = arg.Int(), true
+			case reflect.Float32, reflect.Float64:
+				*p, ok = int64(arg.Float()), true
+			default:
+				return fmt.Errorf("could not parse %v (%s) into %v (%T)", arg, arg.Type(), ptr, ptr)
+			}
+		case *float64:
+			switch arg.Kind() {
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+				*p, ok = float64(arg.Int()), true
+			case reflect.Float32, reflect.Float64:
+				*p, ok = arg.Float(), true
+			default:
+				return fmt.Errorf("could not parse %v (%s) into %v (%T)", arg, arg.Type(), ptr, ptr)
+			}
+		}
+
+		if ok {
+			continue
+		}
+
+		if !arg.CanInterface() {
+			return fmt.Errorf("argument at position %d can't be accessed via Interface()", i)
+		}
+		val := arg.Interface()
+
+		switch p := ptr.(type) {
+		case *interface{}:
+			*p, ok = val, true
+		case *bool:
+			*p, ok = val.(bool)
+		case *string:
+			*p, ok = val.(string)
+		case *time.Time:
+			*p, ok = val.(time.Time)
+		case *[]interface{}:
+			*p, ok = val.([]interface{})
+		case *map[string]interface{}:
+			*p, ok = val.(map[string]interface{})
+		default:
+			return fmt.Errorf("trying to parse %v into %v: unhandled value type %T", arg, p, val)
+		}
+
+		if !ok {
+			return fmt.Errorf("could not parse %v (%s) into %v (%T)", arg, arg.Type(), ptr, ptr)
+		}
+	}
+
+	return nil
 }
 
 // Func function implementing this type is called directly, which is faster than calling through reflect.
