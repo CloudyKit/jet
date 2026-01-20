2686401125e193bd0190df025b9b6364e5413fed
sauerbraten <willing.alexander@googlemail.com>
2026-01-20 18:28:34 +0100

    fix path building in embed.FS loader on Windows
    fixes #226

--- loaders/embedfs/loader.go
+++ loaders/embedfs/loader.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"io"
 	"io/fs"
+	"path"
 	"path/filepath"
 
 	"github.com/CloudyKit/jet/v6"
@@ -11,25 +12,25 @@ import (
 
 type embedFileSystemLoader struct {
 	dir string
-	fs embed.FS
+	fs  embed.FS
 }
 
 // NewLoader returns an initialized loader serving the passed embed.FS.
 func NewLoader(dirPath string, fs embed.FS) jet.Loader {
 	return &embedFileSystemLoader{
-		dir: filepath.FromSlash(dirPath),
-		fs: fs,
+		dir: path.Clean(filepath.ToSlash(dirPath)),
+		fs:  fs,
 	}
 }
 
 // Open implements Loader.Open() on top of an embed.FS.
 func (l *embedFileSystemLoader) Open(name string) (io.ReadCloser, error) {
-	return l.fs.Open(filepath.Join(l.dir, filepath.FromSlash(name)))
+	return l.fs.Open(path.Join(l.dir, path.Clean(filepath.ToSlash(name))))
 }
 
 // Exists implements Loader.Exists() on top of an embed.FS by trying to open the file.
 func (l *embedFileSystemLoader) Exists(name string) bool {
-	name = filepath.Join(l.dir, filepath.FromSlash(name))
+	name = path.Join(l.dir, path.Clean(filepath.ToSlash(name)))
 	stat, err := fs.Stat(l.fs, name)
 	if err == nil && !stat.IsDir() {
 		return true
